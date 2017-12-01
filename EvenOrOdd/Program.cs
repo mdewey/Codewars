96ac2031aa7819cc64c7c2e5759a5ac402e3702c
Mark Dewey <mark.t.dewey@gmail.com>
2017-11-30 21:34:11 -0500

    added demo idea

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create a function that takes an integer as an argument and returns "Even" 
+// for even numbers or "Odd" for odd numbers.
+
+using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        public class SolutionClass
+        {
+            public static string EvenOrOdd(int number) => (number % 2 == 0) ? "Even" : "Odd";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
